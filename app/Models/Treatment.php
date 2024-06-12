bb15f544d4c0bca06a4572549cbf45dcfde4d347
cesar <cesarg@sonora.gob.mx>
2024-06-12 14:34:20 -0700

    Creating a chart widget

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use App\Casts\MoneyCast;
 
 class Treatment extends Model
 {
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
